4937b2f2f9a4945205d7d91373d9909d8c9e9015
fersasil <gfersasil@gmail.com>
2020-08-09 06:47:42 -0300

    change project structure to flutter

---
+++ test/site_cc_parser_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:site_cc_parser/site_cc_parser.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = Calculator();
+    expect(calculator.addOne(2), 3);
+    expect(calculator.addOne(-7), -6);
+    expect(calculator.addOne(0), 1);
+    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  });
+}
